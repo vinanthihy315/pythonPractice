7fc8c498b4e449a80881de5585e2a2dc08490311
Vinanthi <vinanthi@Deepus-MacBook-Pro.local>
2026-05-05 09:51:32 +0530

    Initial commit

---
+++ first.py
@@ -0,0 +1,40 @@
+# print("Hello world!")
+
+
+# name = input("What is your name")
+# print("Welcome here", name)
+
+
+# age = 25
+# year = 2026
+# called_as = "Vin"
+# salary = 232.324
+# char = 'c'
+
+# print(type(age))
+# print(type(year))
+# print(type(called_as))
+# print(type(salary))
+# print(type(char))
+
+# print(2**4)
+
+
+
+# first = "John"
+# last = "Dae"
+# full_name = first + " " + last
+# print(full_name) 
+
+# age = 30
+# message = f"My name is {first} {last} and I am {age} years old"
+# print(message)
+
+text = "  Hello, Python!  "
+print(text)
+print(text.strip())
+print(text.upper())
+print(text.lower())
+print(text.replace("jh", "Hi"))
+print(len(text))
+print(len(text.strip()))
